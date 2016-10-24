1f6825dd9f8a96ce1e7a4225f70704f50c830a77
Anthony <anthonymg1994@gmail.com>
2016-10-24 16:38:40 -0500

    Version 1.0

---
+++ app/src/main/java/com/mx/bridgestudio/kangup/Views/TypesOfAutomobiles.java
@@ -0,0 +1,22 @@
+package com.mx.bridgestudio.kangup.Views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mx.bridgestudio.kangup.R;
+
+/**
+ * Created by USUARIO on 24/10/2016.
+ */
+
+public class TypesOfAutomobiles extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.types);
+
+
+    }
+
+}
