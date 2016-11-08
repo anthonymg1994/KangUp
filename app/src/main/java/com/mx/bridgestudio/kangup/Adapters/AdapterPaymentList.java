6db5febde544370cc46920aa1d2edff4954ee92c
Isaac <thecorre.nexus@gmail.com>
2016-11-08 11:22:05 -0600

    New Payment Activity

---
+++ app/src/main/java/com/mx/bridgestudio/kangup/Adapters/AdapterPaymentList.java
@@ -0,0 +1,49 @@
+package com.mx.bridgestudio.kangup.Adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mx.bridgestudio.kangup.Models.ListCar;
+import com.mx.bridgestudio.kangup.Models.Payment;
+import com.mx.bridgestudio.kangup.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Isaac on 08/11/2016.
+ */
+
+public class AdapterPaymentList extends ArrayAdapter<Payment> {
+
+    Activity context;
+    private ArrayList<Payment> lista;
+    private Payment[] objects;
+    private View listView;
+
+    public AdapterPaymentList(Activity context, int e, ArrayList<Payment> list) {
+        super(context, R.layout.payment_list, list);
+        // TODO Auto-generated constructor stub
+        this.context = (Activity) context;
+        this.lista = lista;
+    }
+    @Override
+    public View getView(int arg0, View arg1, ViewGroup arg2) {
+        // TODO Auto-generated method stub
+        View item = null;
+        ImageView imgImg = null;
+        if (item == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            item = inflater.inflate(R.layout.payment_list, null);
+            imgImg = (ImageView)item.findViewById(R.id.imagePay);
+            TextView Nnom = (TextView) item.findViewById(R.id.textDataPay);
+
+        }
+        return item;
+
+    }
+}
---
+++ app/src/main/java/com/mx/bridgestudio/kangup/Views/PaymentActivity.java
@@ -0,0 +1,40 @@
+package com.mx.bridgestudio.kangup.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mx.bridgestudio.kangup.Adapters.AdaptadorList;
+import com.mx.bridgestudio.kangup.Adapters.AdapterPaymentList;
+import com.mx.bridgestudio.kangup.Models.ListCar;
+import com.mx.bridgestudio.kangup.Models.Payment;
+import com.mx.bridgestudio.kangup.R;
+
+import java.util.ArrayList;
+
+public class PaymentActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listPay;
+    private ArrayList<Payment> tipos = new ArrayList<>();
+    private ArrayAdapter<Payment> AdapterArray;
+    private AdapterPaymentList adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        listPay = (ListView)findViewById(R.id.listPayment);
+        //fillList();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+
+}
